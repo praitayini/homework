9a916a4f6b41e6b0cc3095e0689a3d998e465634
praitayini <praitayini@gmail.com>
2017-09-06 09:02:08 -0400

    Homework

---
+++ prathu.js
@@ -0,0 +1,16 @@
+function call(){
+var x=parseInt(document.getElementById("x").value);
+var y=parseInt(document.getElementById("y").value);
+var z=x+y;
+   if(isNaN(x)||isNaN(y)){
+          alert("please enter a number");
+     }
+     else
+
+        {
+
+         var z=x+y;
+         alert("The sum is " +result);
+       }
+
+}
